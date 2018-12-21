8010a53d5127be1167e4df90bc104370a2c63c25
ouyangxu <1210796299@qq.com>
2018-12-21 16:13:23 +0800

    初次提交

---
+++ src/main/java/com/oyx/sgin/SginApplication.java
@@ -0,0 +1,14 @@
+package com.oyx.sgin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SginApplication.class, args);
+	}
+
+}
+
