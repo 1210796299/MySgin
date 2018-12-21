d6c3f4f0d1dea7b9eefb8789fa78f086182abdcd
ouyangxu <1210796299@qq.com>
2018-12-21 16:17:29 +0800

    2

--- src/main/java/com/oyx/sgin/SginApplication.java
+++ src/main/java/com/oyx/sgin/SginApplication.java
@@ -3,9 +3,10 @@ package com.oyx.sgin;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class SginApplication {
-
+	//测试修改
 	public static void main(String[] args) {
 		SpringApplication.run(SginApplication.class, args);
 	}
